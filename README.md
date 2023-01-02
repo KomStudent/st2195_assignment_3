0a8e69e474a5419102347d30b6e4393fd5f2080c
Karl Munroe <kom9@student.london.ac.uk>
2023-01-02 02:25:50 -0500

    Updating Code and Readme

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ extracted from the Harvard Dataverse (Link:)
 The assignment will develop code to perform data analysis on the airline travel data
 from, and including the years 2000 to 2005. Data analysis will be presented and exported for review
 in CSV file format
+
+## Questions to be Answered
+
+#### 1.	Which of the following companies has the highest number of cancelled flights?
+A.  Delta Air Lines Inc., 107851 cancellations
+
+#### 2. Which of the following airplanes has the lowest associated average departure delay (excluding cancelled and diverted flights)?
+A. 
+
+
+
---
+++ r_sql/airline-analysis.r
@@ -0,0 +1,58 @@
+# Author: Karl Munroe
+# Date: January 1, 2023
+# Title: R Code for ST2195 Assignment 3
+
+#Load DBI Library
+library(DBI)
+
+#Check if database exists. Use absolute path to prevent sync with GitHub Repo
+if (file.exists("c:/DATA/R/airlineInfo.db"))
+  file.remove("c:/DATA/R/airlineInfo.db")
+
+#Create the connection to the Database
+conn <- dbConnect(RSQLite::SQLite(),"c:/DATA/R/airlineInfo.db")
+
+# CSV Files are large only load and write to Database one at a time
+
+# Year 2000
+yr2000 <- read.csv("c:/DATA/2000.csv", header=TRUE)
+dbWriteTable(conn, "ontime", yr2000)
+
+#Year 2001 - Table has been created in step 1. above, append the data to the table here
+yr2001 <- read.csv("c:/DATA/2001.csv", header=TRUE)
+dbAppendTable(conn, "ontime", yr2001)
+
+#Year 2002 - Table has been created in step 1. above, append the data to the table here
+yr2002 <- read.csv("c:/DATA/2002.csv", header=TRUE)
+dbAppendTable(conn, "ontime", yr2002)
+
+#Year 2003 - Table has been created in step 1. above, append the data to the table here
+yr2003 <- read.csv("c:/DATA/2003.csv", header=TRUE)
+dbAppendTable(conn, "ontime", yr2003)
+
+#Year 2004 - Table has been created in step 1. above, append the data to the table here
+yr2004 <- read.csv("c:/DATA/2004.csv", header=TRUE)
+dbAppendTable(conn, "ontime", yr2004)
+
+#Year 2005 - Table has been created in step 1. above, append the data to the table here
+yr2005 <- read.csv("c:/DATA/2005.csv", header=TRUE)
+dbAppendTable(conn, "ontime", yr2005)
+
+# Add in other data tables: planes, airports & carriers
+airports <- read.csv("c:/DATA/airports.csv", header=TRUE)
+carriers <- read.csv("c:/DATA/carriers.csv", header=TRUE)
+plane_data <- read.csv("c:/DATA/plane-data.csv", header=TRUE)
+
+dbWriteTable(conn, "airports", airports)
+dbWriteTable(conn, "carriers", carriers)
+dbWriteTable(conn, "planes", plane_data)
+
+# QUIZ - Question 1
+# Which of the following companies has th"e highest number of cancelled flights?
+CancelledFlights <- dbGetQuery(conn,"SELECT carriers.Description, COUNT(ontime.Cancelled) as Total_Cancelled FROM ontime INNER JOIN carriers ON carriers.Code = ontime.UniqueCarrier WHERE Cancelled = 1 GROUP BY carriers.Description ORDER BY Total_Cancelled DESC")
+CancelledFlights
+head(CancelledFlights,n=1 )
+write.csv(CancelledFlights,"cancelled-flights-DBI.csv")
+#Finally Close the Connection
+dbDisconnect(conn)
+
